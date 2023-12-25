6dc1b468ea9b683809be31cb26a5e07b16953c43
st4l <152074070+St4lV@users.noreply.github.com>
2023-12-26 00:52:33 +0100

    mobile display switch fix

--- src/Header/SocialIcons.js
+++ src/Header/SocialIcons.js
@@ -1,31 +1,24 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faInstagram, faDiscord, faGithub } from '@fortawesome/free-brands-svg-icons';
 function SocialIcons({ isMobile }) {
-  const socialClasses = {
-    barremobile: isMobile ? 'barres-mobile' : 'barres',
-    lieninsta: isMobile ? 'apb_insta-mobile' : 'apb_insta',
-    liendiscord: isMobile ? 'apb_discord-mobile' : 'apb_discord',
-    liengithub: isMobile ? 'apb_github-mobile' : 'apb_github',
-  };
   return (
     <div className={isMobile ? 'social-icons-mobile' : 'social-icons'}>
-      <div className={socialClasses.barres}>
+      <div className={isMobile ? 'apb-barres_mobile' : 'apb_barres'}>
          <FontAwesomeIcon icon="fa-solid fa-bars" /> 
      </div>
       <a href="https://instagram.com/tirnatek" className="lieninsta" target="_blank" rel="noreferrer">
-        <div className={socialClasses.apb_insta}>
+        <div className={isMobile ? 'apb-insta_mobile' : 'apb_insta'}>
           <FontAwesomeIcon icon={faInstagram} />
         </div>
       </a>
-      <FontAwesomeIcon icon="fa-solid fa-bars" />
       <a href="https://discord.gg/GJgnaYJ8jQ" className="liendiscord" target="_blank" rel="noreferrer">
-        <div className={socialClasses.apb_discord}>
+      <div className={isMobile ? 'apb-insta_discord' : 'apb_discord'}>
           <FontAwesomeIcon icon={faDiscord} />
         </div>
       </a>
       
       <a href="https://github.com/St4lV/docker-react-azuracast-web-interface" className="liengithub" target="_blank" rel="noreferrer">
-        <div className={socialClasses.apb_github}>
+      <div className={isMobile ? 'apb-github_mobile' : 'apb_github'}>
           <FontAwesomeIcon icon={faGithub} />
         </div>
       </a>
